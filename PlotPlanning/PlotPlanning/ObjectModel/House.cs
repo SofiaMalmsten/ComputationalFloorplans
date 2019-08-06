3b620c024c23df0dac53f11fc5865a901c0156e6
Sofia Malmsten <sofia.malmsten@architude.se>
2019-08-06 14:44:50 +0200

    Added house class

---
+++ PlotPlanning/PlotPlanning/ObjectModel/House.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+
+namespace PlotPlanning.ObjectModel
+{
+    public class House
+    {
+        public string Type { get; set; } = "";
+
+    }
+
+    //====================================================================
+
+}
