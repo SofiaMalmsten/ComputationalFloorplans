5f1f539b3d3911e4ecbdb385a742ad38f8ece1a7
Erik Forsberg <erikforsberg95@hotmail.com>
2019-08-08 14:35:03 +0200

    Create Cell.cs

---
+++ PlotPlanning/PlotPlanning/Components/Objects/Cell.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace PlotPlanning.Components
+{
+    public class CellComponent : GH_Component
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public CellComponent()
+          : base("GenerateCell", "GenerateCell",
+              "Generate cell",
+              "PlotPlanningTool", "Objects")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddCurveParameter("boundary", "boundary", "boundary", GH_ParamAccess.item);
+           
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("cell", "cell", "cell", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //Create class instances
+            Polyline boundary = new Polyline();            
+
+            //Get Data
+            if (!DA.GetData(0, ref boundary))
+                return;            
+
+            //Set properties
+            PlotPlanning.ObjectModel.Cell cell = new ObjectModel.Cell();
+            if (boundary.IsClosed) cell.BoundaryCurve = boundary;             
+
+            //Set data
+            DA.SetData(0, cell);
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                return Properties.Resources.SnapToTopo;
+                //return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("ce681780-bc81-403c-b6f5-84eb9ed63d81"); }
+        }
+    }
+
+
+}
