66b54ed99e5f5a8d378acb4b86cc7387a514a224
Erik Forsberg <erikforsberg95@hotmail.com>
2019-08-22 15:46:06 +0200

    fix bug

--- PlotPlanning/PlotPlanning/Methods/PlotMethods/PlaceHouseRow.cs
+++ PlotPlanning/PlotPlanning/Methods/PlotMethods/PlaceHouseRow.cs
@@ -16,16 +16,16 @@ namespace PlotPlanning.Methods
         {
             //L<IHouse> a = baseHouses.ToList(); 
             if (baseHouses[0].GetType() == new SingleFamily().GetType())
-            {
-                baseHouses.ConvertAll(obj => obj.(new SingleFamily().GetType()));
-            }
-            return PlaceHouseRow(baseHouses as dynamic, bound, originalBound, roads, random, method, carport);           
 
+                return PlaceHouseRow(baseHouses.Cast<SingleFamily>().ToList(), bound, originalBound, roads, random, method, carport);
+            
+            else
+                
+                return PlaceHouseRow(baseHouses.Cast<MultiFamily>().ToList(), bound, originalBound, roads, random, method, carport);
+                     
         }
-
         
         //SFH
-
 
         public static (List<IHouse>, List<PolylineCurve>, List<Carport>) PlaceHouseRow(List<SingleFamily> baseHouses, Curve bound, Curve originalBound, List<Curve> roads, Random random, string method, Carport carport)
         {
@@ -78,49 +78,51 @@ namespace PlotPlanning.Methods
             return (IHouseList, cutBound, carportList);
         }
 
-
-       
         //MFH
 
-        //public static void PlaceHouseRow(List<MultiFamily> baseHouses, Curve bound, Curve originalBound, List<Curve> roads, Random random, string method, Carport carport, out List<MultiFamily> houseList, out List<PolylineCurve> cutBound, out List<Carport> carportList)
-        //{
-        //    houseList = new List<MultiFamily>();
-        //    carportList = new List<Carport>();
+        public static (List<IHouse>, List<PolylineCurve>, List<Carport>) PlaceHouseRow(List<MultiFamily> baseHouses, Curve bound, Curve originalBound, List<Curve> roads, Random random, string method, Carport carport)
+        {
+            List<MultiFamily> houseList = new List<MultiFamily>();
+            List<Carport> carportList = new List<Carport>();
+            List<PolylineCurve> cutBound = new List<PolylineCurve>();
 
-        //    MultiFamily baseHouse = baseHouses[random.Next(baseHouses.Count)]; //1. pick house type to place
+            MultiFamily baseHouse = baseHouses[random.Next(baseHouses.Count)]; //1. pick house type to place
 
-        //    //2. Get boundaries. 
-        //    bound.TryGetPolyline(out Polyline boundPL);
-        //    List<Line> lines = SegmentBounds(boundPL.ClosePolyline(), baseHouse);
+            //2. Get boundaries. 
+            bound.TryGetPolyline(out Polyline boundPL);
+            List<Line> lines = SegmentBounds(boundPL.ClosePolyline(), baseHouse);
 
-        //    if (lines.Count == 0)
-        //        goto end;
+            if (lines.Count == 0)
+                goto end;
 
-        //    Line currLine = lines.PickLine(method, random, roads, originalBound);
-        //    currLine.Extend(-FilletOffset(), -FilletOffset());
-        //    List<Point3d> possiblePts = PossiblePoints(currLine, baseHouse, random, carport);
+            Line currLine = lines.PickLine(method, random, roads, originalBound);
+            currLine.Extend(-FilletOffset(), -FilletOffset());
+            List<Point3d> possiblePts = PossiblePoints(currLine, baseHouse, random, carport);
 
-        //    //3. Place houses for each position
-        //    for (int i = 0; i < possiblePts.Count; i++)
-        //    {
-        //        MultiFamily movedHouse = Adjust.Translate(baseHouse, possiblePts[i], currLine.Direction);
+            //3. Place houses for each position
+            for (int i = 0; i < possiblePts.Count; i++)
+            {
+                MultiFamily movedHouse = Adjust.Translate(baseHouse, possiblePts[i], currLine.Direction);
 
-        //        if (IsInside(movedHouse, bound)) //TODO: Include carport
-        //            houseList.Add(movedHouse);
-        //        else if (houseList.Count != 0) //already places houses
-        //            break;
-        //    }
+                if (IsInside(movedHouse, bound)) //TODO: Include carport
+                    houseList.Add(movedHouse);
+                else if (houseList.Count != 0) //already places houses
+                    break;
+            }
 
-        //end:
-        //    if (houseList.Count >= baseHouse.MinAmount)
-        //        cutBound = UpdateBoundaries(houseList, baseHouse, bound); //TODO: Include carport
-        //    else
-        //    {
+        end:
+            if (houseList.Count >= baseHouse.MinAmount)
+                cutBound = UpdateBoundaries(houseList, baseHouse, bound); //TODO: Include carport
+            else
+            {
 
-        //        cutBound = new List<PolylineCurve>() { bound.CurveToPolylineCurve() };
-        //        houseList = new List<MultiFamily>();
-        //        carportList = new List<Carport>();
-        //    }
-        //}
+                cutBound = new List<PolylineCurve>() { bound.CurveToPolylineCurve() };
+                houseList = new List<MultiFamily>();
+                carportList = new List<Carport>();
+            }
+
+            List<IHouse> IHouseList = houseList.Cast<IHouse>().ToList();
+            return (IHouseList, cutBound, carportList);
+        }
     }
 }
