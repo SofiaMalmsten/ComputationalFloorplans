97aa037bee9f22eaa74bcc6ce22f7d6bf758a6cf
Sofia Malmsten <sofia.malmsten@architude.se>
2019-08-29 12:07:16 +0200

    Added structure for the node components

--- PlotPlanning/PlotPlanning/Components/Evaluate/MassCalculations.cs
+++ PlotPlanning/PlotPlanning/Components/Evaluate/MassCalculations.cs
@@ -11,7 +11,8 @@ namespace PlotPlanning.Components
 {
     public class MassCalculations : GH_Component
     {
-        //====================================================================//
+
+        #region Register Node
         /// <summary>
         /// Each implementation of GH_Component must provide a public 
         /// constructor without any arguments.
@@ -26,48 +27,6 @@ namespace PlotPlanning.Components
         {
         }
 
-        //====================================================================//
-        #region Outputs
-        /// <summary>
-        /// Registers all the input parameters for this component.
-        /// </summary>
-        protected override void RegisterInputParams(GH_InputParamManager pManager)
-        {
-            pManager.AddGenericParameter("SFH", "S", "SFH", GH_ParamAccess.list);
-        }
-
-        //====================================================================//
-        /// <summary>
-        /// Registers all the output parameters for this component.
-        /// </summary>
-        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
-        {
-            pManager.AddIntegerParameter("NumberOfHouses", "N", "Number Of Houses", GH_ParamAccess.item);
-        }
-
-        //====================================================================//
-        /// <summary>
-        /// This is the method that actually does the work.
-        /// </summary>
-        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
-        /// to store data in output parameters.</param>
-        protected override void SolveInstance(IGH_DataAccess DA)
-        {
-            //Create class instances
-            List<ObjectModel.SingleFamily> SFH = new List<ObjectModel.SingleFamily>();
-
-            //Get Data
-            if (!DA.GetDataList(0, SFH))
-                return;
-
-            //Calculate
-            int numerOfHouses = SFH.Count;
-           
-            //Set data
-            DA.SetData(0, numerOfHouses);
-        }
-
-        //====================================================================//
         /// <summary>
         /// Provides an Icon for every component that will be visible in the User Interface.
         /// Icons need to be 24x24 pixels.
@@ -90,7 +49,51 @@ namespace PlotPlanning.Components
         {
             get { return new Guid("dfd8d506-3ac5-4785-8515-472179549439"); }
         }
+        #endregion
         //====================================================================//
+        #region Inputs/Outputs
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("SFH", "S", "SFH", GH_ParamAccess.list);
+        }
+        
+        //====================================================================//
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddIntegerParameter("NumberOfHouses", "N", "Number Of Houses", GH_ParamAccess.item);
+        }
+        #endregion
+        //====================================================================//
+        #region Solution
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //Create class instances
+            List<ObjectModel.SingleFamily> SFH = new List<ObjectModel.SingleFamily>();
+
+            //Get Data
+            if (!DA.GetDataList(0, SFH))
+                return;
+
+            //Calculate
+            int numerOfHouses = SFH.Count;
+           
+            //Set data
+            DA.SetData(0, numerOfHouses);
+        }
+        #endregion
+
+       
     }
 
 
