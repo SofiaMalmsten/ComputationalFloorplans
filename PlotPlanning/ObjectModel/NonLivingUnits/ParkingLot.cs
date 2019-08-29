ca9c993924405d7d114ac6adf18dfe1f65a4bcb1
Erik Forsberg <erikforsberg95@hotmail.com>
2019-08-29 14:10:30 +0200

    last commit

--- PlotPlanning/ObjectModel/NonLivingUnits/ParkingLot.cs
+++ PlotPlanning/ObjectModel/NonLivingUnits/ParkingLot.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using Rhino.Geometry;
 
-
 namespace PlotPlanning.ObjectModel
 {
     public class ParkingLot
     {
+        #region Properties
         public string Type { get; set; } = "";
         public Curve ParkingBound { get; set; } = new PolylineCurve();
         public int Floors { get; set; } = 0;
+        #endregion
+
+        #region Constructors
+        //TODO:Add constructors
+        #endregion
+
+        #region Public methods
+        //TODO:Add constructors
+        #endregion
+
+        #region Private methods
+        //TODO:Add constructors
+        #endregion
 
     }
 
-    //====================================================================
+    //====================================================================//
 
 }
