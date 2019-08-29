e65769b46a928c69e1c1ade4cd0c760988a489a1
Erik Forsberg <erikforsberg95@hotmail.com>
2019-08-29 11:58:19 +0200

    3 more

--- PlotPlanning/Engine/Geometry/OffsetOut.cs
+++ PlotPlanning/Engine/Geometry/OffsetOut.cs
@@ -1,36 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
-
-using Grasshopper.Kernel;
 using Rhino.Geometry;
-using PlotPlanning.Engine.Base;
 
 namespace PlotPlanning.Engine.Geometry
 {
     public static partial class Adjust
     {
-
         public static Curve OffsetOut(this Curve curve, double distance, Plane plane, CurveOffsetCornerStyle cornerStyle = CurveOffsetCornerStyle.Sharp)
         {
+            if (distance == 0) return curve;
 
-            if (distance == 0) return curve; 
-
-
-            double original_area = Rhino.Geometry.AreaMassProperties.Compute(curve).Area;
-            Curve[] offset_1 = curve.Offset(plane, distance, ObjectModel.Tolerance.Distance,cornerStyle);
+            double original_area = AreaMassProperties.Compute(curve).Area;
+            Curve[] offset_1 = curve.Offset(plane, distance, ObjectModel.Tolerance.Distance, cornerStyle);
             if (offset_1 != null)
             {
-                double area_1 = Rhino.Geometry.AreaMassProperties.Compute(offset_1[0]).Area;
+                double area_1 = AreaMassProperties.Compute(offset_1[0]).Area;
                 if (area_1 > original_area)
-                    return offset_1[0]; 
+                    return offset_1[0];
                 else
                     return curve.Offset(plane, -distance, ObjectModel.Tolerance.Distance, cornerStyle)[0];
             }
             else
-                return curve.Offset(plane, -distance, ObjectModel.Tolerance.Distance, cornerStyle)[0]; 
+                return curve.Offset(plane, -distance, ObjectModel.Tolerance.Distance, cornerStyle)[0];
         }
-        
+
+        //====================================================================//
+        public static NotImplementedException OffsetIn(this Curve curve, double distance, Plane plane, CurveOffsetCornerStyle cornerStyle = CurveOffsetCornerStyle.Sharp)
+        {
+            return new NotImplementedException();
+        }
+
     }
 }
-
-    //====================================================================
