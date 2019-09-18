328d96de5c5bca2b830c98b1225c4cf2a6c66358
Erik Forsberg <erikforsberg95@hotmail.com>
2019-09-18 19:32:40 +0200

    fixed names and added coments

--- PlotPlanning/Engine/Geometry/MoveInside.cs
+++ PlotPlanning/Engine/Geometry/MoveInside.cs
@@ -6,10 +6,10 @@ namespace PlotPlanning.Engine.Geometry
     public static partial class Adjust
     {
         //Moves a point along a vecotr inside a boundary. If the point moves outside the new point will be the closest point on the boundary.
-        public static Point3d MoveInside(this Point3d pt, Vector3d v, Curve bound)
+        public static Point3d MoveInside(this Point3d point, Vector3d vector, Curve bound) //Change the name of this method to something like MoveWithin or MovePointInsideCurve.
         {
-            Point3d testPt = pt + v;
-            Line testLine = new Line(pt, pt + v);
+            Point3d testPt = point + vector;
+            Line testLine = new Line(point, point + vector);
             Point3d movedPt = new Point3d();
 
             //Check wheter the point is inside the building boundary
