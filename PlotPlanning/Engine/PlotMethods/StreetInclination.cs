5e9a676f112c370d8e8d6401a6a62996af366aec
Erik Forsberg <erikforsberg95@hotmail.com>
2019-09-18 22:06:04 +0200

    added method for multiple streets to calculate weighted average

--- PlotPlanning/Engine/PlotMethods/StreetInclination.cs
+++ PlotPlanning/Engine/PlotMethods/StreetInclination.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using PlotPlanning.ObjectModel;
 using Rhino.Geometry;
-using System.Linq; 
+using System.Linq;
 
 namespace PlotPlanning.Methods
 {
@@ -13,17 +13,37 @@ namespace PlotPlanning.Methods
             Point3d[] evaluationPoints = street.DivideEquidistant(distanceBetweenPoints);
             List<double> zValues = evaluationPoints.Select(x => x.Z).ToList();
 
-            double inclination = 0; 
-            for (int i = 0; i < zValues.Count-1; i++)            
+            double inclination = 0;
+            for (int i = 0; i < zValues.Count - 1; i++)
                 inclination += Math.Abs(zValues[i] - zValues[i + 1]);
 
-            inclination /= street.GetLength();   
+            inclination /= street.GetLength();
 
-            return inclination; 
+            return inclination;
         }
 
         //====================================================================//
 
+        public static double StreetInclination(this List<Curve> streets, double distanceBetweenPoints = 1)
+        {
+            double inclination = 0;
+            double totalLength = 0;
+
+            foreach (Curve street in streets)
+            {
+                Point3d[] evaluationPoints = street.DivideEquidistant(distanceBetweenPoints);
+                List<double> zValues = evaluationPoints.Select(x => x.Z).ToList();
+
+                for (int i = 0; i < zValues.Count - 1; i++)
+                    inclination += Math.Abs(zValues[i] - zValues[i + 1]);
+
+                totalLength += street.GetLength();
+            }
+
+            inclination /= totalLength;
+            return inclination;
+
+        }
     }
 }
 
