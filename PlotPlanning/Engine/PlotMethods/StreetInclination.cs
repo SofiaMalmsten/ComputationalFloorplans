b6bbfbe58fcf708bbfe9188a07c8d83fe9c3a4ab
Sofia Malmsten <sofia.malmsten@architude.se>
2019-09-27 18:50:28 +0200

    Added street inclination

---
+++ PlotPlanning/Engine/PlotMethods/StreetInclination.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using PlotPlanning.ObjectModel;
+using Rhino.Geometry;
+using System.Linq;
+
+
+namespace PlotPlanning.Methods
+{
+    public static partial class Evaluate
+    {
+        public static double StreetInclination(this Curve street, double distanceBetweenPoints = 1)
+        {
+
+            Point3d[] evaluationPoints = street.DivideEquidistant(distanceBetweenPoints);
+            List<double> zValues = evaluationPoints.Select(x => x.Z).ToList();
+
+            int ptCount = zValues.Count;
+            double inclination = 0;
+
+            for (int i = 0; i < ptCount - 1; i++)
+                inclination += Math.Abs(zValues[i] - zValues[i + 1]);
+
+            inclination /= ((ptCount - 1) * distanceBetweenPoints);
+            double avgAngle = Math.Asin(inclination) * 180 / Math.PI;
+
+            return avgAngle;
+        }
+        //====================================================================//
+
+        public static double StreetInclination(this List<Curve> streets, double distanceBetweenPoints = 1)
+        {
+            double inclination = 0;
+            int totalPtCount = 0;
+
+            foreach (Curve street in streets)
+            {
+                Point3d[] evaluationPoints = street.DivideEquidistant(distanceBetweenPoints);
+                List<double> zValues = evaluationPoints.Select(x => x.Z).ToList();
+                int ptCount = zValues.Count;
+
+                for (int i = 0; i < ptCount - 1; i++)
+                    inclination += Math.Abs(zValues[i] - zValues[i + 1]);
+
+                totalPtCount += ptCount;
+
+            }
+
+            inclination /= ((totalPtCount - 1) * distanceBetweenPoints);
+            double avgAngle = Math.Asin(inclination) * 180 / Math.PI;
+            return avgAngle;
+
+        }
+    }
+
+}
