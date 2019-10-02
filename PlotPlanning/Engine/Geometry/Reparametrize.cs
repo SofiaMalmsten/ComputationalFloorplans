289964b330e4713a9337a086446d278f0b93915b
Erik Forsberg <erikforsberg95@hotmail.com>
2019-10-02 15:25:11 +0200

    added reparametrize(curve) and started on voronoi network

---
+++ PlotPlanning/Engine/Geometry/Reparametrize.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Rhino.Geometry;
+using System.Collections.Generic; 
+
+namespace PlotPlanning.Engine.Geometry
+{
+    public static partial class Compute
+    {
+        public static void Reparametrize(Curve c)
+        {
+            c.Domain = new Interval(0, 1);
+        }
+
+        public static void Reparametrize(List<Curve> crvs)
+        {
+            foreach (Curve c in crvs)
+                Reparametrize(c);
+        }
+
+        //====================================================================//
+
+    }
+}
---
+++ PlotPlanning/Engine/PlotMethods/VoronoiNetwork.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using PlotPlanning.ObjectModel;
+using Rhino.Geometry;
+using Rhino.NodeInCode;
+using System.Linq; 
+
+namespace PlotPlanning.Methods
+{
+    public static partial class Generate
+    {
+        public static void VoronoiNetwork(List<Point3d> voronoiPoints, Curve offsetBoundary)
+        {
+            
+
+
+        }
+
+        //====================================================================//
+
+        private static List<Curve> Voronoi(List<Point3d> pts)
+        {
+            ComponentFunctionInfo voronoi = Components.FindComponent("Voronoi");
+
+            object[] arguments = new object[]
+              {(object) pts,
+              };
+
+            string[] warnings;
+            object[] result = voronoi.Evaluate(arguments, false, out warnings);
+
+            IList<object> resultList = (IList<object>)result[0];
+            List<Curve> crv_list = resultList.Select(x => (Curve)x).ToList();
+
+            return crv_list;
+        }
+    }
+}
+
+
