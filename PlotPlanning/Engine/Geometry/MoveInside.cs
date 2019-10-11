d0ff4c0a3921967da78251e87c92ca240d12a50e
Sofia Malmsten <sofia.malmsten@architude.se>
2019-10-11 17:06:58 +0200

    Bug fixes + one new GH component

--- PlotPlanning/Engine/Geometry/MoveInside.cs
+++ PlotPlanning/Engine/Geometry/MoveInside.cs
@@ -7,17 +7,14 @@ namespace PlotPlanning.Engine.Geometry
     public static partial class Adjust
     {
         //Moves a point along a vecotr inside a boundary. If the point moves outside the new point will be the closest point on the boundary.
-        public static Point3d MoveInside(this Point3d point, Vector3d vector, Curve bound) //Change the name of this method to something like MoveWithin or MovePointInsideCurve.
+        public static Point3d MoveInside(this Point3d point, Vector3d vector, Curve bound) //Change the name of this method
         {
             Point3d testPt = point + vector;
             Point3d movedPt = new Point3d();
 
             //Check wheter the point is inside the building boundary
-
             if (bound.Contains(testPt, Plane.WorldXY, 0.01) == Rhino.Geometry.PointContainment.Inside)
-            {
                 movedPt = testPt;
-            }
             else
             {
                 Line l = new Line(point, testPt);
@@ -29,4 +26,6 @@ namespace PlotPlanning.Engine.Geometry
             return movedPt;
         }
     }
+
+    //====================================================================//
 }
---
+++ PlotPlanning/PlotPlanning/Components/Testing/GetClosestValue.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino.Geometry;
+using Grasshopper.Kernel;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace PlotPlanning.Components
+{
+    public class GetClosestValue : GH_Component
+    {
+        #region Register node
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public GetClosestValue()
+          : base("GetClosestValue", "GetClosestValue",
+              "GetCLosestValue",
+              "PlotPlanningTool", "Testing")
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return Properties.Resources.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("b1ee20fa-228a-4698-9499-58c3eee30852"); }
+        }
+
+        #endregion
+
+        #region Input/Output
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddNumberParameter("ValueToCheck", "V", "V", GH_ParamAccess.item);
+            pManager.AddNumberParameter("ReferenceList", "R", "R", GH_ParamAccess.list);
+
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddNumberParameter("N", "N", "Number", GH_ParamAccess.item);
+        }
+
+        #endregion
+
+        #region Solution
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        /// 
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //Create class instances
+            double valueToCheck = 0;
+            List<double> refValues = new List<double>();
+            double number = 0;
+
+            //Get Data
+            if (!DA.GetData(0, ref valueToCheck))
+                return;
+            if (!DA.GetDataList(1, refValues))
+                return;
+
+
+            //Calculate
+            number = PlotPlanning.Engine.Base.Modify.ClosestValue(valueToCheck, refValues);
+
+            //Set data
+            DA.SetData(0, number);
+        }
+
+        #endregion
+    }
+}
