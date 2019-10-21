23a4b08d10f90c67ff18b4862cb4d88c8d81e9fc
Erik Forsberg <erikforsberg95@hotmail.com>
2019-10-21 15:16:56 +0200

    added ExpandRectangle() for the gardens

---
+++ PlotPlanning/Engine/Geometry/ExpandRectangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Rhino.Geometry;
+
+namespace PlotPlanning.Engine.Geometry
+{
+    public static partial class Convert
+    {
+        public static Rectangle3d ExpandRectangle(Rectangle3d rec, double front, double back)
+        {
+            rec.MakeIncreasing();
+            Point3d pt1 = rec.Corner(0);
+            pt1 = new Point3d(pt1.X - front, pt1.Y, pt1.Z);
+            Point3d pt2 = rec.Corner(2);
+            pt2 = new Point3d(pt2.X + back, pt2.Y, pt2.Z);
+            Rectangle3d expandedRectangle = new Rectangle3d(new Plane(rec.Center, Vector3d.ZAxis), pt1, pt2);           
+
+            return expandedRectangle; 
+        }
+
+        //====================================================================//
+    }
+}
