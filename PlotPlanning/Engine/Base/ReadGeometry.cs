bf45b1fe52c20664037bcf89c4d4b9c258581255
Erik Forsberg <erikforsberg95@hotmail.com>
2019-10-21 16:03:15 +0200

    added the old readGeometry method back in to make it work for now

--- PlotPlanning/Engine/Base/ReadGeometry.cs
+++ PlotPlanning/Engine/Base/ReadGeometry.cs
@@ -7,7 +7,7 @@ namespace PlotPlanning.Engine.Base
 {
     public class ReadGeometry
     {
-        public static (Brep, Point3d, Rectangle3d) ReadHouseGeometry(string houseType)
+        public static (Brep, Point3d, Rectangle3d) ReadAllHouseGeometry(string houseType)
         {
 
 
@@ -20,9 +20,9 @@ namespace PlotPlanning.Engine.Base
             string rec_string = splitStrings[2];
 
             Brep brep = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(brep_string)) as Brep;
-            Point point = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(brep_string)) as Point;
+            Point point = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(point_string)) as Point;
             Point3d point3d = point.Location; 
-            PolylineCurve rec_plc = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(brep_string)) as PolylineCurve;
+            PolylineCurve rec_plc = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(rec_string)) as PolylineCurve;
 
             Polyline rec_pl = new Polyline();
             rec_plc.TryGetPolyline(out rec_pl);
@@ -31,14 +31,16 @@ namespace PlotPlanning.Engine.Base
 
             return (brep, point3d, rec);
 
-            /*
+        }
+
+        //====================================================================//
+        public static Brep ReadHouseGeometry(string houseType)
+        {
             string fileName = ReadResourceFile("Engine.Resources." + houseType + ".txt");
             GeometryBase geometry = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(fileName));
             return geometry as Brep;
-            */
-
         }
-        
+
         //============            Private Methods               ==============//        
 
         private static string ReadResourceFile(string filename)
