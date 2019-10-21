e1e703f06080fbee739e0bd334e99e52862a821d
Erik Forsberg <erikforsberg95@hotmail.com>
2019-10-21 15:11:34 +0200

    changed the readgeometry method to return a tuple of multiple objects

--- PlotPlanning/Engine/Base/ReadGeometry.cs
+++ PlotPlanning/Engine/Base/ReadGeometry.cs
@@ -7,11 +7,35 @@ namespace PlotPlanning.Engine.Base
 {
     public class ReadGeometry
     {
-        public static Brep ReadHouseGeometry(string houseType)
+        public static (Brep, Point3d, Rectangle3d) ReadHouseGeometry(string houseType)
         {
+
+
+            string fileName = ReadResourceFile("Engine.Resources." + houseType + ".txt");
+            char[] separator = { '$' };
+
+            string[] splitStrings = fileName.Split(separator);
+            string brep_string = splitStrings[0];
+            string point_string = splitStrings[1];
+            string rec_string = splitStrings[2];
+
+            Brep brep = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(brep_string)) as Brep;
+            Point point = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(brep_string)) as Point;
+            Point3d point3d = point.Location; 
+            PolylineCurve rec_plc = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(brep_string)) as PolylineCurve;
+
+            Polyline rec_pl = new Polyline();
+            rec_plc.TryGetPolyline(out rec_pl);
+            Rectangle3d rec =  new Rectangle3d(Plane.WorldXY, rec_pl[0], rec_pl[2]);
+
+
+            return (brep, point3d, rec);
+
+            /*
             string fileName = ReadResourceFile("Engine.Resources." + houseType + ".txt");
             GeometryBase geometry = GH_Convert.ByteArrayToCommonObject<GeometryBase>(System.Convert.FromBase64String(fileName));
             return geometry as Brep;
+            */
 
         }
         
