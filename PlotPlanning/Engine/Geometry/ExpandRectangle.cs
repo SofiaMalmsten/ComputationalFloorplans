13201b230614b25968fb52d1bd2adf71b1829304
Erik Forsberg <erikforsberg95@hotmail.com>
2019-10-23 16:39:01 +0200

    added new recource files for testing

--- PlotPlanning/Engine/Geometry/ExpandRectangle.cs
+++ PlotPlanning/Engine/Geometry/ExpandRectangle.cs
@@ -9,12 +9,12 @@ namespace PlotPlanning.Engine.Geometry
         {
             rec.MakeIncreasing();
             Point3d pt1 = rec.Corner(0);
-            pt1 = new Point3d(pt1.X - front, pt1.Y, pt1.Z);
-            Point3d pt2 = rec.Corner(2);
-            pt2 = new Point3d(pt2.X + back, pt2.Y, pt2.Z);
-            Rectangle3d expandedRectangle = new Rectangle3d(new Plane(rec.Center, Vector3d.ZAxis), pt1, pt2);           
+            pt1 = new Point3d(pt1.X, pt1.Y - front, pt1.Z);
+            Point3d pt2 = rec.Corner(3);
+            pt2 = new Point3d(pt2.X, pt2.Y + back, pt2.Z);
+            Rectangle3d expandedRectangle = new Rectangle3d(new Plane(rec.Center, Vector3d.ZAxis), pt1, pt2);
 
-            return expandedRectangle; 
+            return expandedRectangle;
         }
 
         //====================================================================//
